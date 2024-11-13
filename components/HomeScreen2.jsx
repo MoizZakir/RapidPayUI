2fc5dd41bda7c73fafc33aee272dd3747dafe8ad
MoizZakir <moizzakir87@gmail.com>
2024-11-13 10:39:42 +0500

    'scr2-done'

---
+++ components/HomeScreen2.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../style/HomeScr2.css'
+
+const HomeScreen2 = () => {
+  return (
+    <div className='HomeScreen2'>
+        <h3>Trusted by the World's best companies</h3>
+        <div className='company'>
+            
+                <img src="https://nearpay.io/_ipx/w_640,q_75/%2F_next%2Fstatic%2Fmedia%2Fmada.ecb4c84d.png?url=%2F_next%2Fstatic%2Fmedia%2Fmada.ecb4c84d.png&w=640&q=75" alt="" />
+                
+                <img src="https://nearpay.io/_next/static/media/visa.6b96beb2.svg" alt="" />
+                <img src="https://nearpay.io/_next/static/media/master.9f887cc9.svg" alt="" />
+                <img src="https://nearpay.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Famerican.41de6ed1.png&w=128&q=75" alt="" />
+                <img src="https://nearpay.io/_next/static/media/pci.b17eeff7.svg" alt="" />
+                
+
+        </div>
+        <div class="box">
+        <h1>Welcome!</h1>
+        <p>This box has a black background with a blurry purple color at the bottom center.</p>
+    </div>
+
+
+    </div>
+  )
+}
+
+export default HomeScreen2
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HomeScreen1 from '../components/HomeScreen1'
+import HomeScreen2 from '../components/HomeScreen2'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <HomeScreen1/>
+        <HomeScreen2/>
     </div>
   )
 }
