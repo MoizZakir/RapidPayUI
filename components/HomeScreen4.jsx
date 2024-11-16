d0be8a6665da8c345bbc2febcb5e1fc6e1ccebf2
MoizZakir <moizzakir87@gmail.com>
2024-11-17 01:42:34 +0500

    'screen4-done'

--- components/HomeScreen4.jsx
+++ components/HomeScreen4.jsx
@@ -7,16 +7,28 @@ import { SiKotlin } from 'react-icons/si'
 import { GrSwift } from 'react-icons/gr'
 
 const HomeScreen4 = () => {
+  const text=` 
+  nearpay.purchase(amount, object : PaymentListener {
+  override fun onPaymentApproved(result: PaymentResult) {
+      //your implementation
+  }
+  override fun onPaymentDeclined(result: PaymentResult) {
+      //your implementation${<br/>}
+  }
+  override fun onPaymentError(errorType: PaymentError) {
+      //your implementation
+  }
+});`
   return (
     <div className='homescreen4'>
      <div className="homescreen4-section1">
         <div>
         <h1>We support all platforms & languages</h1>
     <p>Whether Java or Kotlin, our updated SDK supports your app. We prioritize security and functionality to match evolving standards.</p>     
-    <p style={{color:'black'}}>Platform</p>   
+    <p style={{color:'black', marginTop:'25px'}}>Platform</p>   
         </div>
 
-<div style={{ width:'90%', paddingRight:'60px', display:'flex',justifyContent:'space-between'}}>
+<div className='pltform' style={{  display:'flex',gap:"50px", flexWrap:'wrap', paddingRight:'20px'}}>
 <RiAndroidFill color='gray'size={60} />
 <FaApple color='gray'size={60} />
 <FaChrome  color='gray'size={60} />
@@ -24,8 +36,8 @@ const HomeScreen4 = () => {
 
 
 </div>
-    <p style={{color:'black'}}>Languages & Frameworks</p>   
-<div className='frameworks' style={{ border:'1px solid black', display:'flex',gap:"30px", flexWrap:'wrap', paddingRight:'20px'}}>
+    <p style={{color:'black', marginTop:'25px'}}>Languages & Frameworks</p>   
+<div className='frameworks' style={{ display:'flex',gap:"30px", flexWrap:'wrap', paddingRight:'20px'}}>
 <FaJava color='gray'size={60} />
 <SiKotlin  color='gray'size={60} />
 <GrSwift color='gray'size={60} />
@@ -38,7 +50,15 @@ const HomeScreen4 = () => {
 </div>
    
         </div>   
-     <div className="homescreen4-section2"></div>   
+     <div className="homescreen4-section2">
+      <h2 style={{marginBottom:"10px"}}>RapidPay SDK</h2>
+      <textarea value={text} readOnly={true} style={{backgroundColor:'black' ,color:"white",width:'90%', height:"400px",fontSize:'19px',padding:'20px',borderRadius:'10px',border:'none'
+        
+      }} name="" id="">
+        
+      </textarea>
+      
+      </div>   
         
         
         </div>
